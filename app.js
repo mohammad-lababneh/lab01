7c11c7e83de27e5b37130062291f942992b7ad0d
mohammad-lababneh <mohammadlababneh96@gmail.com>
2021-05-01 02:57:57 +0300

    resubmit lab01

---
+++ app.js
@@ -0,0 +1,35 @@
+let username = prompt('please enter your name');
+
+
+customername( username);
+
+function customername(username)
+{
+alert('Hello '+ username);
+
+  let userAge = prompt(username + ' please enter your age?');
+
+
+alert (username + ' your age is ' + userAge);
+
+
+let  watchtype = prompt('do you like watches ? yes or no')
+if (watchtype === 'yes' ){
+
+alert ('that is amazing , watches can give you amazing style')
+
+}else { 
+
+  alert ('you should change your maind')
+}
+
+
+
+let evaluation = prompt( ' what is your evaluation for our servise ? '  )
+alert ('thank you very much ' + username )
+
+console.log('  please enter your name  ' + username);
+console.log('  please enter your age?  ' + userAge);
+console.log('  do you like watches ? yes or no  ' + watchtype);
+console.log('  what is your evaluation for our servise ?   ' +  evaluation);
+}
